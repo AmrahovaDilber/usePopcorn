96ad221bce536c603fc0b2831abe196ae50399b4
AmrahovaDilber <amrahovad911@gmail.com>
2024-07-02 01:18:03 +0400

    added Loading

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,9 @@ import Movie from "./Movie";
 function Movies({ movies }) {
   return (
   
-      <div className="scrool-smooth p-5 rounded-xl w-full md:w-1/2 mb-4 md:mb-0 bg-[#343a40]">
+      <div
+      style={{ height: "calc(100vh - 150px)" }} 
+          className="overflow-scroll p-5 rounded-xl w-full md:w-1/2 mb-4 md:mb-0 bg-[#343a40] h-[calc(100vh - 100px)]">
         {movies.map((movie) => (
           <Movie key={movie.id} movie={movie}></Movie>
         ))}
